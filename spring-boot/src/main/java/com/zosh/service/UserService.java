3a89c4b444362316846c0001677cc415eb6e334c
Nithin <nknithinabc@gmail.com>
2024-02-29 15:18:57 +0530

    first commit

---
+++ spring-boot/src/main/java/com/zosh/service/UserService.java
@@ -0,0 +1,9 @@
+package com.zosh.service;
+
+import com.zosh.exception.UserException;
+import com.zosh.modal.User;
+
+public interface UserService {
+	public User findUserById(Long userId) throws UserException;
+	public User findUserProfileByJwt(String jwt) throws UserException;
+}
